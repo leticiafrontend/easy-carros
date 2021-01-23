7e49653c09d98edfec2c2ef7397975a9e6417a46
leticiafrontend <leticia.alesilva@outlook.com>
2021-01-23 12:15:27 -0300

    :lipstick: - Creating notification component

---
+++ src/components/Notification/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NotificationContainer, NotificationContent } from './style';
+
+export const Notification = () => {
+  return (
+    <NotificationContainer>
+      <NotificationContent>
+        <h2>Agendado com sucesso</h2>
+      </NotificationContent>
+    </NotificationContainer>
+  );
+};
---
+++ src/components/Notification/style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const NotificationContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 2000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NotificationContent = styled.div`
+  background-color: #e8ffe9;
+  border: 1px solid #02e64a;
+  color: #02e64a;
+  border-radius: 4px;
+  padding: 40px;
+  box-shadow: 2px 2px 10px #00000029;
+  position: relative;
+`;
